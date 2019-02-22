90df86d7f0b7e27e1ef9f2cca4d789500570cdfe
lukasz_marek <lukasz.marek1@ibm.com>
2019-02-22 12:52:14 +0100

    implemented simple vocabularyregistry

--- src/main/java/com/randomcorp/processing/vocabulary/VocabularyRegistryImpl.java
+++ src/main/java/com/randomcorp/processing/vocabulary/VocabularyRegistryImpl.java
@@ -1,14 +1,36 @@
 package com.randomcorp.processing.vocabulary;
 
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class VocabularyRegistryImpl implements VocabularyRegistry {
+public final class VocabularyRegistryImpl implements VocabularyRegistry {
+
+    private final AtomicLong idGenerator = new AtomicLong(0);
+
+    private final ConcurrentHashMap<String, Word> idRegistry = new ConcurrentHashMap<>();
 
     public Word registerAsWord(String word){
-        return null;
+        if (idRegistry.containsKey(word)){
+            return idRegistry.get(word);
+        }
+
+        return registerIfAbsent(word);
     }
+
 
     public Optional<Word> getRegisteredWord(String word){
         return Optional.empty();
+    }
+
+
+    private synchronized Word registerIfAbsent(String word){
+        if(!idRegistry.containsKey(word)){
+            final Word newWord = new Word(idGenerator.getAndIncrement(), word);
+            idRegistry.put(word, newWord);
+            return newWord;
+        }
+
+        return idRegistry.get(word);
     }
 }
