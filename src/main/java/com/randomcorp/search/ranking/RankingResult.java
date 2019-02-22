439832493523e846c56e8942a5276bbe8875fa90
lukasz_marek <lukasz.marek1@ibm.com>
2019-02-22 15:48:21 +0100

    implemented ranking result

---
+++ src/main/java/com/randomcorp/search/ranking/RankingResult.java
@@ -0,0 +1,18 @@
+package com.randomcorp.search.ranking;
+
+public final class RankingResult {
+
+    private final int value;
+
+    public RankingResult(int value){
+        if (value < 0 || value > 100){
+            throw new IllegalArgumentException(String.format("Value must be between 0 and 100, %d was given.", value));
+        }
+
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
--- src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
+++ src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
@@ -1,4 +1,9 @@
 package com.randomcorp.search.ranking;
 
+import com.randomcorp.search.SearchResult;
+
 public interface RankingStrategy {
+
+    RankingResult rank(SearchResult searchResult);
+
 }
