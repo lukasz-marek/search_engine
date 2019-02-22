e433383e679d10ae5d6d94e1156c3ff841d8f7d1
lukasz_marek <lukasz.marek1@ibm.com>
2019-02-22 15:49:23 +0100

    added Query to rank() method

--- src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
+++ src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
@@ -1,9 +1,10 @@
 package com.randomcorp.search.ranking;
 
+import com.randomcorp.search.Query;
 import com.randomcorp.search.SearchResult;
 
 public interface RankingStrategy {
 
-    RankingResult rank(SearchResult searchResult);
+    RankingResult rank(SearchResult searchResult, Query query);
 
 }
