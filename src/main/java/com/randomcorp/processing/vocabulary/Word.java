6ff1ce38f58465778b63ec2ddd4cf5c8b58b549e
lukasz_marek <lukasz.marek1@ibm.com>
2019-02-22 13:42:23 +0100

    implemented hashcode and equals for Word

--- src/main/java/com/randomcorp/processing/vocabulary/Word.java
+++ src/main/java/com/randomcorp/processing/vocabulary/Word.java
@@ -1,5 +1,7 @@
 package com.randomcorp.processing.vocabulary;
 
+import java.util.Objects;
+
 public final class Word {
 
     public String getValue() {
@@ -17,5 +19,22 @@ public final class Word {
     public Word(long id, String value){
         this.id = id;
         this.value = value;
+    }
+
+    @Override
+    public int hashCode(){
+        return Long.valueOf(this.id).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o){
+
+        if(!(o instanceof Word)){
+            return false;
+        }
+
+        final Word asWord = (Word) o;
+
+        return Objects.equals(this.id, asWord.id) && Objects.equals(this.value, asWord.value);
     }
 }
