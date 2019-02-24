7a1b3bcfba7029bcd11b86ececf53b7bf4c9a63b
lmarek <marek.lucas@gmail.com>
2019-02-24 23:48:13 +0100

    code cleanup - RankingStrategy transformed into Template Method

--- src/main/java/com/randomcorp/search/ranking/DefaultRankingStrategy.java
+++ src/main/java/com/randomcorp/search/ranking/DefaultRankingStrategy.java
@@ -5,26 +5,15 @@ import com.randomcorp.search.matching.Query;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
-public class DefaultRankingStrategy implements RankingStrategy {
+public final class DefaultRankingStrategy extends RankingStrategy {
 
-    @Override
-    public RankingResult rank(List<Match> matches, Query query) {
-        final Optional<Integer> longestMatch = matches.stream().map(Match::getOccurrences).map(List::size).max(Comparator.naturalOrder());
-
-        if (!longestMatch.isPresent()) {
-            return new RankingResult(0);
-        } else if (longestMatch.get() == query.getWords().size()) {
-            return new RankingResult(100);
-        }
-
-        return compute(matches, query);
-
-    }
-
-    protected RankingResult compute(List<Match> matches, Query query) {
-        final int longestMatch = matches.stream().map(Match::getOccurrences).map(List::size).max(Comparator.naturalOrder()).get();
+    protected final RankingResult computeRankingForPartialMatch(List<Match> matches, Query query) {
+        final int longestMatch = matches.stream()
+                .map(Match::getOccurrences)
+                .map(List::size)
+                .max(Comparator.naturalOrder())
+                .get();
 
         final double coverage = ((double) longestMatch) / ((double) query.getWords().size());
         final int rank = (int) (coverage * 100.0);
--- src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
+++ src/main/java/com/randomcorp/search/ranking/RankingStrategy.java
@@ -3,10 +3,24 @@ package com.randomcorp.search.ranking;
 import com.randomcorp.search.matching.Match;
 import com.randomcorp.search.matching.Query;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
-public interface RankingStrategy {
+public abstract class RankingStrategy {
 
-    RankingResult rank(List<Match> matches, Query query);
+    public final RankingResult rank(List<Match> matches, Query query) {
+        final Optional<Integer> longestMatch = matches.stream().map(Match::getOccurrences).map(List::size).max(Comparator.naturalOrder());
+
+        if (!longestMatch.isPresent()) {
+            return new RankingResult(0);
+        } else if (longestMatch.get() == query.getWords().size()) {
+            return new RankingResult(100);
+        }
+
+        return computeRankingForPartialMatch(matches, query);
+    }
+
+    protected abstract RankingResult computeRankingForPartialMatch(List<Match> matches, Query query);
 
 }
