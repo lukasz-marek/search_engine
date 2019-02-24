1aea05c155d7eeae4f87e050f1056106bd7546b0
lmarek <marek.lucas@gmail.com>
2019-02-24 18:13:09 +0100

    code cleanup

--- src/test/java/com/randomcorp/file/image/FileImageTest.java
+++ src/test/java/com/randomcorp/file/image/FileImageTest.java
@@ -16,33 +16,45 @@ public class FileImageTest {
 
     @Test
     public void shouldContainWordsInCorrectPlaces() throws IOException {
+        // given
         final ClassLoader classLoader = FileImageTest.class.getClassLoader();
         final File file = new File(classLoader.getResource(FILE_NAME).getFile());
         final VocabularyRegistry registry = new VocabularyRegistryImpl(new IdentityWordNormalizer());
+
+        // when
         final FileImage fileImage = FileImage.of(file, registry, new WhitespaceLineSplitter());
 
+        // then
         Assert.assertTrue(fileImage.getWordIndexes().get(registry.getRegisteredWord("General")).contains(0l));
         Assert.assertTrue(fileImage.getWordIndexes().get(registry.getRegisteredWord("Kenobi.")).contains(1l));
     }
 
     @Test
     public void shouldBeCaseSensitive() throws IOException {
+        // given
         final ClassLoader classLoader = FileImageTest.class.getClassLoader();
         final File file = new File(classLoader.getResource(FILE_NAME).getFile());
         final VocabularyRegistry registry = new VocabularyRegistryImpl(new IdentityWordNormalizer());
+
+        // when
         final FileImage fileImage = FileImage.of(file, registry, new WhitespaceLineSplitter());
 
+        // then
         Assert.assertFalse(fileImage.getWordIndexes().containsKey(registry.getRegisteredWord("general")));
         Assert.assertFalse(fileImage.getWordIndexes().containsKey(registry.getRegisteredWord("kenobi.")));
     }
 
     @Test
     public void shouldContainCorrectNumberOfPositions() throws IOException {
+        // given
         final ClassLoader classLoader = FileImageTest.class.getClassLoader();
         final File file = new File(classLoader.getResource(FILE_NAME).getFile());
         final VocabularyRegistry registry = new VocabularyRegistryImpl(new IdentityWordNormalizer());
+
+        // when
         final FileImage fileImage = FileImage.of(file, registry, new WhitespaceLineSplitter());
 
+        // then
         Assert.assertEquals(fileImage.getWordIndexes().get(registry.getRegisteredWord("Kenobi.")).size(), 2);
     }
 }
